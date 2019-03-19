a54fb0f7b9360a042e0d504c1f9266818a34c509
LeedWover <aresone.gaben@gmail.com>
2019-03-19 14:37:08 +0100

    init next app

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const indexPage = () => (
+  <div>
+    <h1>home page</h1>
+  </div>
+)
+
+export default indexPage;
---
+++ pages/users/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const usersPage = () => (
+  <div>
+    <h1>users page</h1>
+  </div>
+)
+
+export default usersPage;
