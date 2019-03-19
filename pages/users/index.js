c70121aa6bd493100ac10ae8a39db786622a07fa
LeedWover <aresone.gaben@gmail.com>
2019-03-19 14:59:49 +0100

    code-splitting

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from '../../components/user';
 
-const usersPage = () => (
-  <div>
-    <h1>users page</h1>
-    <User name="Gábor" age="25" />
-  </div>
-)
+class usersPage extends Component {
+  state = {
+    users: [
+      {
+        name: "Gábor",
+        age: 25
+      },
+      {
+        name: "Zsolt",
+        age: 47
+      },
+      {
+        name: "Ági",
+        age: 16
+      }
+    ]
+  };
+
+  
+  render() {
+    let showUsers = this.state.users.map((user, index) => {
+      return (
+        <User key={index} name={user.name} age={user.age} />
+      );
+    });
+
+    return (
+      <div>
+        <h1>users page</h1>
+        {showUsers}
+      </div>
+    )
+  }
+}
 
 export default usersPage;
